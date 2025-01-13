924d3e2974fe1773ec39dfdfc4f0076c6379a86a
Jakub Pietrucha <elpietruch@gmail.com>
2025-01-13 19:36:04 +0100

    second initial commit

--- tests/test_colors.py
+++
@@ -1,9 +0,0 @@
-from modules.tokens import get_color_for_token
-
-def test_get_color_for_token():
-    assert get_color_for_token('TRAIL_FOREST_') == 'green'
-    assert get_color_for_token('TRAIL_WATER_') == 'blue'
-    assert get_color_for_token('TRAIL_MOUNTAIN') == 'grey'
-    assert get_color_for_token('WEAKNESS_FOREST_') == 'green'
-    assert get_color_for_token('WEAKNESS_WATER_') == 'blue'
-    assert get_color_for_token('WEAKNESS_MOUNTAIN') == 'grey'
